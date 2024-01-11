0880b07fb041730f1d078323dc2fdae87ad1e4cc
UnsurpasseDD <138458940+UnsurpasseDD@users.noreply.github.com>
2024-01-11 19:15:17 +1100

    Add files via upload

---
+++ NewsPortal/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('news_portal.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('sign/', include('sign.urls')),
+    path('pages/', include('django.contrib.flatpages.urls')),
+]
